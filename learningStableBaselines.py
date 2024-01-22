55de91679fcdd401915cc813ab08f8975f182cbb
Farhan Hasan <hasanfarhan33@gmail.com>
2024-01-22 21:19:41 +0000

    end of vid 1

--- learningStableBaselines.py
+++ learningStableBaselines.py
@@ -1,11 +1,20 @@
 import gymnasium as gym 
+from stable_baselines3 import PPO
 
-env = gym.make("LunarLander-v2")
 
-env.reset() 
+env = gym.make("LunarLander-v2", render_mode = "human")
+observation, info = env.reset() 
 
-print("sample action: ", env.action_space.sample())
-print("observation space shape: ", env.observation_space.shape)
-print("sample observation: ", env.observation_space.sample())
+model = PPO("MlpPolicy", env, verbose = 1)
+model.learn(total_timesteps=10000)
 
+episodes = 10 
+for ep in range(episodes):
+    observation, info = env.reset()
+    done = False 
+    
+    while not done: 
+        env.render()
+        observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+    
 env.close() 
