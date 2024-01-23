ca7323fe6acc5588fd6b8641a0a5e82231891415
Farhan Hasan <hasanfarhan33@gmail.com>
2024-01-23 17:02:48 +0000

    trained the models again with GPU

--- learningStableBaselines.py
+++ learningStableBaselines.py
@@ -1,9 +1,10 @@
 import gymnasium as gym 
-from stable_baselines3 import A2C
+from stable_baselines3 import PPO, A2C
 import os 
+import time 
 
-models_dir = "models/A2C"
-logdir = "logs"
+models_dir = f"models/A2C-{int(time.time())}"
+logdir = f"logs/A2C-{int(time.time())}"
 
 if not os.path.exists(models_dir):
     os.makedirs(models_dir)
@@ -14,24 +15,31 @@ if not os.path.exists(logdir):
 env = gym.make("LunarLander-v2", render_mode = "human")
 observation, info = env.reset() 
 
-model = A2C("MlpPolicy", env, verbose = 1, tensorboard_log=logdir)
 
+# Loading a saved model --> loading a PPO model at 50k 
+# models_dir = "models/A2C"
+# model_path = f"{models_dir}/50000.zip"
+# model = A2C.load(model_path, env=env)
+
+# Loading a scratch model 
+model = A2C("MlpPolicy", env, verbose = 1, tensorboard_log=logdir, device = "cuda")
+
+# TRAINING MODEL
 TIMESTEPS = 10000
-
-for i in range(30):
+for i in range(1, 100):
     model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name="A2C")
     model.save(f"{models_dir}/{TIMESTEPS*i}")
 
-'''
-episodes = 10 
-for ep in range(episodes):
-    observation, info = env.reset()
-    done = False 
+# episodes = 10 
+# for ep in range(episodes):
+#     observation, info = env.reset()
+#     done = False 
     
-    while not done: 
-        env.render()
-        observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+#     while not done: 
+#         env.render()
+#         action, _ = model.predict(observation)
+#         observation, reward, terminated, truncated, info = env.step(action)
     
  
-'''
+
 env.close()
